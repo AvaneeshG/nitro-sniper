76b3256d93897846aab26e825bdbf60afba4506e
Avaneesh Naik <avaneeshnaik2663@gmail.com>
2022-02-03 11:37:13 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -7,7 +7,6 @@ function compareVersions {
 
 function installAll() {
   echo "$(tput setaf 6)Installing packages, please wait..."
-  npm init -y && npm i --save-dev node@16 && npm config set prefix=$(pwd)/node_modules/node && export PATH=$(pwd)/node_modules/node/bin:$PATH
   install-pkg "http://archive.ubuntu.com/ubuntu/pool/main/g/glib2.0/libglib2.0-0_2.56.4-0ubuntu0.18.04.8_amd64.deb" &> /dev/null
   echo "$(tput setaf 2)Installed dependencies."
   echo "$(tput setaf 6)Cloning the latest sniper code..."
@@ -18,6 +17,7 @@ function installAll() {
   echo "$(tput setaf 2)Cloned latest version of the sniper."
   echo "$(tput setaf 6)Installing sniper dependencies..."
   npm install &> /dev/null
+  npm i --save-dev node@17 && npm config set prefix=$(pwd)/node_modules/node && export PATH=$(pwd)/node_modules/node/bin:$PATH
   echo "$(tput setaf 2)Installed sniper dependencies."
   echo "$(tput setaf 2)Running sniper..."
   npx node ./src/index.js
